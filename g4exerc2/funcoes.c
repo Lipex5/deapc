5459c28ed2fd75a387fe77d2a6182ea2462d4561
Lipex5 <manuelfilipe3000@hotmail.com>
2019-04-12 09:59:46 +0100

    Some minor changes mid class

--- g4exerc2/funcoes.c
+++ g4exerc2/funcoes.c
@@ -14,19 +14,19 @@ char cifra(char letra, int desvio)
     if(letra == ' ')
         return ' ';
 
-    else if(L > 96 && L < 123) // a - z
+    else if((L > 96) && (L < 123)) // a - z
         if(result > 122)
             result -= 26;
 
-    else if(L > 64 && L < 91) // A - Z
+    else if((L > 64) && (L < 91)) // A - Z
         if(result > 90)
             result -= 26;
-    
+
     else
     {
-        //printf("Wrong character!");
-        result = '_';  // <------- CHEKC THIS!!! NOT WORKING!!!
-    }
+        printf("Wrong character!");
+        result = 95;  // <------- CHECK THIS!!! NOT WORKING!!! 
+    } 
 
     return (char) result;
 }
@@ -34,14 +34,14 @@ char cifra(char letra, int desvio)
 int cifrastring(char *str, int desvio)
 {
     int num = 0;
-    char *final = (char*) malloc(strlen(str));
+    char *final = (char*) malloc(strlen(str) + 1);
 
     for(int i = 0; i < strlen(str); i++)
     {
         *(final + i) = cifra(*(str + i), desvio);
-        if(*(final + i) != '_' && *(final + i) != ' ')
-        num++;
+        if((int) *(final + i) != 95 && *(final + i) != ' ')
+            num++;
     }
 
     return num;
-}
+} 
--- g4exerc2/test.c
+++ g4exerc2/test.c
@@ -3,5 +3,12 @@
 
 int main()
 {
-    printf("%d\n", cifrastring("Hello world!!", 1));
+    char string[12] = "Hello you..";
+    
+    for(int i = 0; i < 12; i++)
+    {
+        printf("%c", cifra(string[i], 3));
+    }
+
+    printf("%d\n", cifrastring("Hello world..", 1));
 }
